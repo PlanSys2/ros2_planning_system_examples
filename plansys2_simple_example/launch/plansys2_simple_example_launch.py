f1b9009c005005d2a42ce1021ab75b00c865d42b
Francisco Martin Rico <fmrico@gmail.com>
2020-03-26 11:01:31 +0100

    Linting
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

--- plansys2_simple_example/launch/plansys2_simple_example_launch.py
+++ plansys2_simple_example/launch/plansys2_simple_example_launch.py
@@ -17,7 +17,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
