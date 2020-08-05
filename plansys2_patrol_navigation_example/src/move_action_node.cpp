70391925ada20911edf111336b6aeb0c9fef5c94
Francisco Martin Rico <fmrico@gmail.com>
2020-08-05 11:21:37 +0200

    Fix CI to foxy
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

--- plansys2_patrol_navigation_example/src/move_action_node.cpp
+++ plansys2_patrol_navigation_example/src/move_action_node.cpp
@@ -81,8 +81,8 @@ public:
 
     navigation_action_client_ =
       rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(
-        shared_from_this(),
-        "navigate_to_pose");
+      shared_from_this(),
+      "navigate_to_pose");
 
     bool is_action_server_ready = false;
     do {
