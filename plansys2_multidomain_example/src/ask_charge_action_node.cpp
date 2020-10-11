f89494d97164051922a82c7c729e5921bfac02a4
Francisco Martin Rico <fmrico@gmail.com>
2020-10-11 14:29:41 +0200

    Adaptation to the BT executor
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

--- plansys2_multidomain_example/src/ask_charge_action_node.cpp
+++ plansys2_multidomain_example/src/ask_charge_action_node.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <memory>
+#include <algorithm>
 
 #include "plansys2_msgs/action/execute_action.hpp"
 
@@ -21,37 +22,45 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
+using namespace std::chrono_literals;
+
 class AskCharge : public plansys2::ActionExecutorClient
 {
 public:
   AskCharge()
-  : plansys2::ActionExecutorClient("askcharge")
+  : plansys2::ActionExecutorClient("askcharge", 1s)
   {
-    getFeedback()->progress = 0.0;
+    progress_ = 0.0;
   }
 
 private:
-  void actionStep()
+  void do_work()
   {
-    if (getFeedback()->progress < 100.0) {
-      getFeedback()->progress += 5.0;
+    if (progress_ < 1.0) {
+      progress_ += 0.05;
+      send_feedback(progress_, "AskCharge running");
+    } else {
+      finish(true, 1.0, "AskCharge completed");
+
+      progress_ = 0.0;
+      std::cout << std::endl;
     }
 
     std::cout << "\r\e[K" << std::flush;
-    std::cout << "Requesting for charging ... [" << getFeedback()->progress << "%]  " <<
+    std::cout << "Requesting for charging ... [" << std::min(100.0, progress_ * 100.0) << "%]  " <<
       std::flush;
   }
 
-  bool isFinished()
-  {
-    return getFeedback()->progress >= 100.0;
-  }
+  float progress_;
 };
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<AskCharge>();
+
+  node->set_parameter(rclcpp::Parameter("action", "askcharge"));
+  node->trigger_transition(lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE);
 
   rclcpp::spin(node->get_node_base_interface());
 
