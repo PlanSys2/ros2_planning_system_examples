0ee3959cb67faccd84bc82a59e1333d6bf23b027
Francisco Martín Rico <fmrico@gmail.com>
2022-07-28 09:07:08 +0200

    Fix CI for Humble
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- plansys2_bt_example/src/behavior_tree_nodes/Move.cpp
+++ plansys2_bt_example/src/behavior_tree_nodes/Move.cpp
@@ -20,7 +20,7 @@
 #include "plansys2_bt_example/behavior_tree_nodes/Move.hpp"
 
 #include "geometry_msgs/msg/pose2_d.hpp"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
 #include "behaviortree_cpp_v3/behavior_tree.h"
 
