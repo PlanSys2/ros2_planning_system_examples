f099c05df44dc06ccf8b2312d2dbf1a6fd103ed9
Josh Zapf <jjzapf@gmail.com>
2022-11-05 23:05:40 -0700

    Adding match example.

---
+++ plansys2_match_example/src/mend_fuse_action_node.cpp
@@ -0,0 +1,69 @@
+// Copyright 2019 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include <algorithm>
+
+#include "plansys2_executor/ActionExecutorClient.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+using namespace std::chrono_literals;
+
+class MendFuseAction : public plansys2::ActionExecutorClient
+{
+public:
+  MendFuseAction()
+  : plansys2::ActionExecutorClient("mend_fuse", 250ms)
+  {
+    progress_ = 0.0;
+    duration_ = std::chrono::duration<double>(0);
+  }
+  
+  private:
+  void do_work()
+  {
+    if (progress_ < 1.0) {
+      progress_ += 0.02;
+      send_feedback(progress_, "Mend fuse running");
+    } else {
+      finish(true, 1.0, "Mend fuse completed");
+      progress_ = 0.0;
+      std::cout << std::endl;
+    }
+
+    std::cout << "\r\e[K" << std::flush;
+    std::cout << "Mend fuse ... [" << std::min(100.0, progress_ * 100.0) << "%]  " <<
+      std::flush;
+  }
+
+  float progress_;
+  std::chrono::duration<double> duration_;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<MendFuseAction>();
+
+  node->set_parameter(rclcpp::Parameter("action_name", "mend_fuse"));
+  node->trigger_transition(lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE);
+
+  rclcpp::spin(node->get_node_base_interface());
+
+  rclcpp::shutdown();
+
+  return 0;
+}
